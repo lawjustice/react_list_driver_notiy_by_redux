0ff327c19096620fef0d8f1458f9050aef05d755
Alvian Rahman Hanif <alvian524@gmail.com>
2018-04-11 23:05:04 +0700

    add LOAD_DRIVER n UPDATE_DRIVER action

--- src/actions/types.js
+++ src/actions/types.js
@@ -1,4 +1,5 @@
 export const FETCH_POSTS = 'FETCH_POSTS';
 export const NEW_POST = 'NEW_POST';
+export const LOAD_DRIVER = 'LOAD_DRIVER';
 export const SELECT_DRIVER = 'SELECT_DRIVER';
-
+export const UPDATE_DRIVER = 'UPDATE_DRIVER'
--- src/reducers/postReducer.js
+++ src/reducers/postReducer.js
@@ -1,5 +1,9 @@
 
-import { SELECT_DRIVER } from '../actions/types';
+import {
+  SELECT_DRIVER,
+  UPDATE_DRIVER,
+  LOAD_DRIVER
+} from '../actions/types';
 
 const initialState = {
 //   items: [],
@@ -10,19 +14,45 @@ const initialState = {
 
 export default function(state = initialState, action) {
   switch (action.type) {
+    case LOAD_DRIVER:
+      return {
+        ...state,
+        drivers: action.payload
+      }
     case SELECT_DRIVER:
         console.log("action.payload from reducer = " + JSON.stringify(action.payload));
-        state = {
-            selectedDriver: action.payload,
-            message : "",
-            isModalShown : true
+        return {
+          ...state,
+          selectedDriver: action.payload,
+          isModalShown: true
         }
-      return state; 
+      //   state = {
+      //       selectedDriver: {
+      //         drivers: state.drivers,
+      //         ...action.payload
+      //       },
+      //       message : "",
+      //       isModalShown : true
+      //   }
+      // return state;
     // case NEW_POST:
     //   return {
     //     ...state,
     //     item: action.payload
     //   };
+    case UPDATE_DRIVER:
+      let newList = state.drivers
+      state.drivers.map((item, index) => {
+        if (item.id === action.payload.id) {
+          newList[index].suspended = 1
+        }
+      })
+      let newState = {
+        ...state,
+       drivers: newList
+      }
+      console.log(newState)
+      return newState
     default:
     console.log("action.payload default = " + JSON.stringify(action.payload));
       return state;
